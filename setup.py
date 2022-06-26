ade4debd78aef846d44646b36d9eaa9cb85429ae
SHALAKA VAZE <shalakavaze1810@gmail.com>
2022-06-26 22:21:49 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 #Declaring variables for setup function
@@ -7,7 +7,6 @@ NAME = "Housing-Predictor"
 VERSION = "0.0.1"
 AUTHOR = "SHALAKA VAZE"
 DESCRIPTION="This is a houseing price prediction app which will predict the prices of the houses on given attributes"
-PACKAGES = ["housing"]
 REQUIREMENT_FILE_NAME = "requirements.txt"
 
 def get_requirements_list()->List[str]:
@@ -16,7 +15,7 @@ def get_requirements_list()->List[str]:
     in the requirements.txt file and return those names as the list"""
 
     with open(REQUIREMENT_FILE_NAME) as requirement:
-        return requirement.readlines()
+        return requirement.readlines().remove("-e .")
 
 
 setup(
@@ -24,9 +23,8 @@ setup(
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(),
     install_requires=get_requirements_list()
-
 )
 
 """ if __name__=="__main__":
